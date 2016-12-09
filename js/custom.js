4eac48bcf9e49dda8402ab24fc1fc9198e6e8228
Filippo Quacquarelli <quacquarelli.filippo@gmail.com>
2016-12-09 14:03:07 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,9 @@
     PRE LOADER
   -------------------------------------------------------------------------------*/
   $(document).ready(function(){
-    $( "#menu-tpl" ).load( "/mysite/templates/nav.html" );
-    $( "#portfolio-tpl" ).load( "/mysite/templates/portfolio.html" );
-    $( "#footer-tpl" ).load( "/mysite/templates/footer.html" );
+    $( "#menu-tpl" ).load( "http://filippoquacquarelli.it/templates/nav.html" );
+    $( "#portfolio-tpl" ).load( "http://filippoquacquarelli.it/templates/portfolio.html" );
+    $( "#footer-tpl" ).load( "http://filippoquacquarelli.it/templates/footer.html" );
   });
 
   $(window).load(function(){
